e22f2fc60a6eebd6f1b2053781d70f6b1016d977
Rikukuku <ricky_gland821_0714@yahoo.co.jp>
2020-01-13 15:41:55 +0900

    Use material-ui

--- src/components/Envents.js
+++ src/components/Envents.js
@@ -4,6 +4,15 @@ import { readEvents } from '../actions'
 import { Link } from 'react-router-dom'
 import map from 'lodash/map'
 
+// material-ui
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Fab from '@material-ui/core/Fab';
+import AddIcon from '@material-ui/icons/Add';
+
 class Events extends Component {
   componentDidMount(){
     this.props.readEvents()
@@ -11,33 +20,35 @@ class Events extends Component {
 
   renderEvents() {
     return map(this.props.events, event => (
-      <tr key={event.id}>
-        <td>{event.id}</td>
-        <td>
+      <TableRow key={event.id}>
+        <TableCell align="right">{event.id}</TableCell>
+        <TableCell align="right">
           <Link to={`/event/${event.id}`}>
             {event.title}
           </Link>
-        </td>
-        <td>{event.body}</td>
-      </tr>
+        </TableCell>
+        <TableCell align="right">{event.body}</TableCell>
+      </TableRow>
     ))
   }
   render() {
     return (
     <React.Fragment>
-      <table>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>title</th>
-            <th>body</th>
-          </tr>
-        </thead>
-        <tbody>
+      <Table aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell align="right">ID</TableCell>
+            <TableCell align="right">title</TableCell>
+            <TableCell align="right">body</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
           {this.renderEvents()}
-        </tbody>
-      </table>
-      <Link to='/event/form'>New Event</Link>
+        </TableBody>
+      </Table>
+      <Fab color="primary" aria-label="add" href="/event/form">
+        <AddIcon />
+      </Fab>
     </React.Fragment>
     )
   }
